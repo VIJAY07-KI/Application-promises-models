56e9eb103179cb93d8e613b789735372eb997828
VIJAY07-KI <bandapallivijay1612@gmail.com>
2024-09-13 20:49:33 +0530

    Promise update

--- models/mongodb-methods.js
+++ models/mongodb-methods.js
@@ -1,18 +1,20 @@
-let find = new Promise(function(resolve,reject){
-    let contactList = [{
-            name: "Vijay",
-            contactNo: "1234567"
-        },
-        {
-            name: "Ganesh",
-            contactNo: "890890908"
-        }]
-    if (contactList.length) {
-        resolve(contactList)
-    } else {
-        reject("operation is failed as contact list not found")
-    }
-})
+function find() {
+    new Promise(function(resolve,reject){
+        let contactList = [{
+                name: "Vijay",
+                contactNo: "1234567"
+            },
+            {
+                name: "Ganesh",
+                contactNo: "890890908"
+            }]
+        if (contactList.length) {
+            resolve(contactList)
+        } else {
+            reject("operation is failed as contact list not found")
+        }
+    })
+}
 
 module.exports = { 
     find
